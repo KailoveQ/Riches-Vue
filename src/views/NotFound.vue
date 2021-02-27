3840346d01685c207e2fc7d3b2c64f21881cf469
Kai <1114366961@qq.com>
2021-02-27 10:28:22 +0800

    添加404页面NotFound

---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <p>404老铁！啥也没有，检查路径对不对</p>
+  <div>
+    <router-link to="/money">返回首页</router-link>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export default class NotFound extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
